dafa0a3ae006e87ac60a1d3ffa7aa980144f9616
unknown <ctim@equinor.com>
2020-05-08 11:01:49 +0200

    fix(minor): redirect uri variable

--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import  { acquireTokenSilent, signInRedirect }  from './auth/AuthFunctions';
 import { UserAgentApplication }  from 'msal';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const myMSALObj = new UserAgentApplication({
-  auth: {
-    clientId: "e90cbb61-896e-4ec7-aa37-23511700e1ed",
-    authority: "https://login.microsoftonline.com/3aa4a235-b6e2-48d5-9195-7fcf05b459b0",
-    redirectUri: process.env.REACT_APP_SEPES_REDIRECT_URI,
-  },
-  cache: {
-    cacheLocation: "sessionStorage", // This configures where your cache will be stored
-    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
-  }
-});
-
-console.log(process.env.REACT_APP_SEPES_REDIRECT_URI);
+import { myMSALObj } from './auth/AuthConfig';
 
 if(myMSALObj.getCurrentConfiguration().cache && !myMSALObj.getAccount()){
   signInRedirect();
