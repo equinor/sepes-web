c859f8654a8e52fe4937113a068173418c1fea12
Hans Kristian Vik Olsen <hanol@equinor.com>
2020-05-13 14:18:30 +0200

    remove home component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,34 +1,8 @@
-import React, {useEffect, useState} from 'react';
-import * as api from '../../services/Api';
+import React from 'react';
 
 const Home = () => {
-    const [isSubscribed, setIsSubscribed] = useState<boolean>(true);
-    const [studyList, setStudyList] = useState([]);
-    const [loading, setLoading] = useState<boolean>(false);
-    
-    /* Commented out to prevent errors when testing on dev. Same as clicking button
-    useEffect(() => {
-        setIsSubscribed(true);
-        getStudyList();
-        return () => setIsSubscribed(false);
-    }, []);
-    */
-    const getStudyList = () => {
-        setLoading(true);
-        api.callStudyList().then((result:any) => {
-            if(isSubscribed){
-                setStudyList(result);
-                console.log("result: " + result)
-            }
-            else{
-                console.log("Err");
-            }
-            setLoading(false);
-        })
-    }
-    
     return (
-        <div><h2>In Development</h2><button onClick={() => getStudyList()}>Send request to Back-End</button>Response: {studyList}</div>
+        <div> </div>
     )
 }
 
