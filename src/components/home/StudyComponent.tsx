22fb3f704315774beccc1d05fd10857c51d78477
Hans Kristian Vik Olsen <hanol@equinor.com>
2020-05-22 15:28:19 +0200

    feat: added description to homepage and made it responsive

--- src/components/home/StudyComponent.tsx
+++ src/components/home/StudyComponent.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Card, Container } from '@equinor/eds-core-react'
 import styled from 'styled-components';
 
 const Dot = styled.span`
@@ -14,39 +13,39 @@ const Dot = styled.span`
     font-size:3em;
   `;
 
-  const Title = styled.span`
+const Title = styled.span`
    margin-left:30px;
    font-weight: bold;
   `;
 
-  const Description = styled.span`
+const Description = styled.span`
     float:right;
   `;
 
-  const SmallText = styled.span`
+const SmallText = styled.span`
     font-size:10px;
   `;
 
-  const Wrapper = styled.div`
+const Wrapper = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fit,minmax(100px,1fr));
+    grid-template-columns: minmax(100px,350px) 1fr;
     width: 100%;
     grid-gap: 10px;
 `;
-//repeat(auto-fit, minmax(19rem, 1fr))
-const StudyComponent = (props:any) => {
-    return (
-        <div style={{backgroundColor: "white", marginLeft: "20px", marginBottom: "10px", display: "flex", borderRadius: "4px", padding: "16px", minWidth: "120px"}}>
-          <Wrapper>
-            <div>
-              <Dot style={{float:"left"}}>SP</Dot>
-              <Title>{props.name}</Title>
-            </div>
-            
-            <p>{props.description}</p>      
-          </Wrapper>
+//repeat(auto-fit,minmax(100px,1fr));
+const StudyComponent = (props: any) => {
+  return (
+    <div style={{ backgroundColor: "white", marginLeft: "20px", marginBottom: "10px", display: "flex", borderRadius: "4px", padding: "16px", minWidth: "120px" }}>
+      <Wrapper>
+        <div>
+          <Dot style={{ float: "left" }}>SP</Dot>
+          <Title>{props.name}</Title>
         </div>
-    )
+
+        <p>{props.description}</p>
+      </Wrapper>
+    </div>
+  )
 }
 
 export default StudyComponent; 
