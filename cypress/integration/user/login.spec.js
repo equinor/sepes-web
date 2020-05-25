bdbf2890fdf54a08ccd6a48a5e4b3e8a8ccf8c54
Daniel Gunnlaugsson <dathgs@hotmail.com>
2020-05-25 15:26:59 +0200

    test: added Cypress, plugins and code coverage

---
+++ cypress/integration/user/login.spec.js
@@ -0,0 +1,9 @@
+describe('Login spec', function () {    
+    before(() => {
+        //cy.login();
+    });
+
+    beforeEach(function () {
+        //Cypress.Cookies.preserveOnce('cyToken');
+    });
+});
