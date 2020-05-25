36d74ae0337bca5257e696a8e0e4bdc7dcfcaacd
Daniel Gunnlaugsson <dathgs@hotmail.com>
2020-05-25 16:08:07 +0200

    combined cypress and jest coverage report

--- cypress/integration/user/login.spec.js
+++ cypress/integration/user/login.spec.js
@@ -6,4 +6,8 @@ describe('Login spec', function () {
     beforeEach(function () {
         //Cypress.Cookies.preserveOnce('cyToken');
     });
+
+    it('visits page', () => {
+        cy.visit('/');
+    });
 });
