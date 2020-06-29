4b7df8331b209aeaf00eaf5ebd6da2ca15097ce4
Hans Kristian Vik Olsen <hanol@equinor.com>
2020-06-29 12:24:50 +0200

    feat(studydetails): user can now upload a photo

---
+++ src/components/common/Dropzone.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useDropzone } from 'react-dropzone';
+import styled from 'styled-components';
+
+const ChooseImgdiv = styled.div`
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-style: groove;
+  background: #eeeeee;
+  @media (max-width: 700px) {
+    width: 95%;
+  }
+`;
+interface props {
+  onDrop: any;
+  accept: string;
+}
+const Dropzone = ({ onDrop, accept }: props) => {
+  // Initializing useDropzone hooks with options
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept,
+  });
+
+  /* 
+    useDropzone hooks exposes two functions called getRootProps and getInputProps
+    and also exposes isDragActive boolean
+  */
+
+  return (
+    <ChooseImgdiv {...getRootProps()}>
+      <input className="dropzone-input" {...getInputProps()} />
+      <div className="text-center">
+        {isDragActive ? (
+          <p className="dropzone-content">Drop here</p>
+        ) : (
+          <p className="dropzone-content">
+            Click or drag n drop photo
+          </p>
+        )}
+      </div>
+    </ChooseImgdiv>
+  );
+};
+
+export default Dropzone;
