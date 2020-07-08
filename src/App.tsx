8aa55497fd24d6ac81561fb8f615d479a2ff7b32
Hans Kristian Vik Olsen <hanol@equinor.com>
2020-07-08 11:44:46 +0200

    refactor(app.tsx): removed console.log

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import './styles/app.scss';
 
 const App = () => {
-    console.log(process.env);
     const appInsights = new ApplicationInsights({ config: {
         instrumentationKey: process.env.REACT_APP_INSTRUMENTATION_KEY
       } });
