df45ad4efeb24f7a7edd405f02de34b31885f079
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-01-05 11:33:46 +0100

    refactor: remove unused code

--- src/components/studyDetails/Tables/DatasetsTable.test.tsx
+++ src/components/studyDetails/Tables/DatasetsTable.test.tsx
@@ -1,32 +1,29 @@
-
 import React from 'react';
-import { render, getByTestId } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import DatasetsTable from './DatasetsTable';
-import { screen } from '@testing-library/dom'
 
+const checkIfDatasetIsAlreadyAdded = (id: string) => {};
 
-const checkIfDatasetIsAlreadyAdded = (id:string) => {
-}
-
-const datasetsList = [{
-    name: 'test1',
-    id: '1'
-},
-{
-    name: 'test2',
-    id: '2'
-}];
+const datasetsList = [
+    {
+        name: 'test1',
+        id: '1'
+    },
+    {
+        name: 'test2',
+        id: '2'
+    }
+];
 test('renders dropdown component', () => {
-    
-  const mockCallBack = jest.fn();
-  const {getByText} = render(
-    <DatasetsTable
-                datasets={datasetsList}
-                removeDataset={checkIfDatasetIsAlreadyAdded}
-                editMode={true}
-                studyId={1}
-                />);
-  let linkElement = getByText('test1');
-  expect(linkElement).toBeInTheDocument();
-  
+    const mockCallBack = jest.fn();
+    const { getByText } = render(
+        <DatasetsTable
+            datasets={datasetsList}
+            removeDataset={checkIfDatasetIsAlreadyAdded}
+            editMode={true}
+            studyId={1}
+        />
+    );
+    let linkElement = getByText('test1');
+    expect(linkElement).toBeInTheDocument();
 });
