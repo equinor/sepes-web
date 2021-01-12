e931e1b70400ab027b4302bf8604a4aa11cdfcd8
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-01-12 12:22:38 +0100

    whip: more warnings

--- src/components/studyDetails/Tables/ParticipantTable.test.tsx
+++ src/components/studyDetails/Tables/ParticipantTable.test.tsx
@@ -1,37 +1,31 @@
-
 import React from 'react';
-import { render, getByTestId } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import ParticipantTable from './ParticipantTable';
+/* eslint-disable no-undef */
+const mockFunc = (id: string) => {};
 
-const mockFunc = (id:string) => {
-}
-
-const participantsList = [{
-    fullName: 'test1',
-    emailAddress: 'test1@test.com',
-    role: 'admin',
-    id: '1'
-},
-{
-    fullName: 'test2',
-    emailAddress: 'test2@test.com',
-    role: 'viewer',
-    id: '2'
-}];
+const participantsList = [
+    {
+        fullName: 'test1',
+        emailAddress: 'test1@test.com',
+        role: 'admin',
+        id: '1'
+    },
+    {
+        fullName: 'test2',
+        emailAddress: 'test2@test.com',
+        role: 'viewer',
+        id: '2'
+    }
+];
 test('renders dropdown component', () => {
-    
-  const mockCallBack = jest.fn();
-  const {getByText} = render(
-    <ParticipantTable 
-        participants={participantsList}
-        removeParticipant={mockFunc}
-        editMode={true}
-    />);
-  let linkElement = getByText('test1');
-  expect(linkElement).toBeInTheDocument();
-  linkElement = getByText('test2@test.com');
-  expect(linkElement).toBeInTheDocument();
-  linkElement = getByText('viewer');
-  expect(linkElement).toBeInTheDocument();
-  
+    const { getByText } = render(
+        <ParticipantTable participants={participantsList} removeParticipant={mockFunc} editMode />
+    );
+    let linkElement = getByText('test1');
+    expect(linkElement).toBeInTheDocument();
+    linkElement = getByText('test2@test.com');
+    expect(linkElement).toBeInTheDocument();
+    linkElement = getByText('viewer');
+    expect(linkElement).toBeInTheDocument();
 });
