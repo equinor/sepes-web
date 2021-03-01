50d9f6f7b9d2595c53c6128d7fb57707de5859cb
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-03-01 10:53:31 +0100

    fix(logos): fixed issue where logos got stretched out when they were not perfect squares
    Closes #641

--- src/components/common/CustomLogoComponent.tsx
+++ src/components/common/CustomLogoComponent.tsx
@@ -2,26 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Logo = styled.img`
-  height: 125px;
-  width: 125px;
+    max-width: 125px;
+    max-height: 125px;
 `;
-
 const Dot = styled.span`
     height: 125px;
     width: 125px;
-    background-color: #EAEAEA;
+    background-color: #eaeaea;
     border-radius: 50%;
     display: inline-block;
     text-align: center;
-    color: #FFFFFF;
+    color: #ffffff;
     line-height: 125px;
-    font-size:3em;
-  `;
+    font-size: 3em;
+`;
 
 const CustomLogoComponent = (props: any) => {
-    return (
-        props.logoUrl ? <Logo src={props.logoUrl} alt='studyLogo' /> : <Dot>SP</Dot>
-    )
-}
+    return props.logoUrl ? <Logo src={props.logoUrl} alt="studyLogo" /> : <Dot>SP</Dot>;
+};
 
 export default CustomLogoComponent;
