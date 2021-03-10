ff90f1515a0f87eb55a7d768153bc30d2325202e
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-03-10 12:04:33 +0100

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,6 @@ module.exports = {
     },
     plugins: ['react'],
     rules: {
-        'linebreak-style': ['error', process.env.NODE_ENV === 'prod' ? 'unix' : 'windows'],
         'react/jsx-filename-extension': [0],
         'eslint/indent:': 0,
         indent: ['off', 4],
