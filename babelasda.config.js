69f17b9cb6f342e9381e3d6b878d21ebca28b0c8
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-03-18 14:03:02 +0100

    wip

--- babelasda.config.js
+++ babelasda.config.js
@@ -1,6 +1,9 @@
 // babel.config.js
+module.exports = {};
+/*
 module.exports = {
     presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
     ignore: ['/node_modules/'],
     plugins: [['@babel/plugin-proposal-class-properties']]
 };
+*/
