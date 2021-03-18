71f665487456b9075e878864dd56a219ad8168d0
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-03-18 12:27:52 +0100

    wip

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 // babel.config.js
 module.exports = {
     presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
-    ignore: ['/node_modules/'],
-    babel: { presets: ['react', 'es2015'] }
+    ignore: ['/node_modules/']
 };
