73cbfd45eced37feb75e9ce36a976dba1949f256
Hans Kristian Vik Olsen <35257765+HansKristianVikOlsen@users.noreply.github.com>
2021-03-24 14:10:46 +0100

    test: rbac study details (#724)
    * test: rbac study details

--- src/tests/component_tests/study/StudyComponentFull.test.tsx
+++ src/tests/component_tests/study/StudyComponentFull.test.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import StudyComponentFull from '../../../components/studyDetails/StudyComponentFull';
-import { StudyObj } from '../common/interfaces';
+import { StudyObj } from '../../../components/common/interfaces';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
+
+const permissions = {
+    addRemoveDataset: true,
+    addRemoveParticipant: true,
+    addRemoveSandbox: true,
+    closeStudy: true,
+    deleteStudy: true,
+    readResulsAndLearnings: true,
+    updateMetadata: true,
+    updateResulsAndLearnings: true
+};
+
+const permissionsAllFalse = {
+    addRemoveDataset: false,
+    addRemoveParticipant: false,
+    addRemoveSandbox: false,
+    closeStudy: false,
+    deleteStudy: false,
+    readResulsAndLearnings: false,
+    updateMetadata: false,
+    updateResulsAndLearnings: false
+};
+
+const permissionsDeleteNotAllowed = {
+    addRemoveDataset: true,
+    addRemoveParticipant: true,
+    addRemoveSandbox: true,
+    closeStudy: true,
+    deleteStudy: false,
+    readResulsAndLearnings: true,
+    updateMetadata: true,
+    updateResulsAndLearnings: true
+};
 
 const study: StudyObj = {
     name: 'StudyName',
@@ -16,7 +49,8 @@ const study: StudyObj = {
     resultsAndLearnings: 'We learned a lot',
     datasets: [],
     participants: [],
-    sandboxes: []
+    sandboxes: [],
+    permissions
 };
 
 const studyWithInvalidName: StudyObj = {
@@ -30,7 +64,8 @@ const studyWithInvalidName: StudyObj = {
     resultsAndLearnings: 'We learned a lot',
     datasets: [],
     participants: [],
-    sandboxes: []
+    sandboxes: [],
+    permissions
 };
 
 const studyWithInvalidVendor: StudyObj = {
@@ -44,7 +79,58 @@ const studyWithInvalidVendor: StudyObj = {
     resultsAndLearnings: 'We learned a lot',
     datasets: [],
     participants: [],
-    sandboxes: []
+    sandboxes: [],
+    permissions
+};
+
+const studyWithNoPermissions: StudyObj = {
+    name: 'StudyName',
+    vendor: '',
+    wbsCode: 'abc123',
+    restricted: false,
+    description: 'Study about fishes',
+    logoUrl: 'asdwasdasd',
+    id: '1',
+    resultsAndLearnings: 'We learned a lot',
+    datasets: [],
+    participants: [],
+    sandboxes: [],
+    permissions: permissionsAllFalse
+};
+
+const studyWithNoDelete: StudyObj = {
+    name: 'StudyName',
+    vendor: '',
+    wbsCode: 'abc123',
+    restricted: false,
+    description: 'Study about fishes',
+    logoUrl: 'asdwasdasd',
+    id: '1',
+    resultsAndLearnings: 'We learned a lot',
+    datasets: [],
+    participants: [],
+    sandboxes: [],
+    permissions: permissionsDeleteNotAllowed
+};
+
+const studyWithSandboxes: StudyObj = {
+    name: 'StudyName',
+    vendor: '',
+    wbsCode: 'abc123',
+    restricted: false,
+    description: 'Study about fishes',
+    logoUrl: 'asdwasdasd',
+    id: '1',
+    resultsAndLearnings: 'We learned a lot',
+    datasets: [],
+    participants: [],
+    sandboxes: [
+        {
+            id: 1,
+            name: 'sandbox1'
+        }
+    ],
+    permissions
 };
 
 const loading = false;
@@ -159,6 +245,137 @@ test('renders StudyComponent full component with new study', () => {
     expect(1).toBe(1);
 });
 
-test('renders StudyComponent full component with new study', () => {
-    expect(1).toBe(1);
+test('renders StudyComponent full component with no permission to edit', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+        <Router history={history}>
+            <StudyComponentFull
+                study={studyWithNoPermissions}
+                newStudy={false}
+                setNewStudy={mockFunc}
+                setLoading={mockFunc}
+                loading={loading}
+                setStudy={mockFunc}
+                setHasChanged={mockFunc}
+                cache={mockFunc}
+                setUpdateCache={mockFunc}
+                updateCache={mockFunc}
+                setDeleteStudyInProgress={mockFunc}
+            />
+        </Router>
+    );
+
+    expect(getByText('Edit').closest('button').disabled).toBeTruthy();
+});
+
+test('renders StudyComponent full component with  permission to delete', () => {
+    const history = createMemoryHistory();
+    const { getByText, getByRole, getByTitle, container, getByTestId } = render(
+        <Router history={history}>
+            <StudyComponentFull
+                study={study}
+                newStudy={false}
+                setNewStudy={mockFunc}
+                setLoading={mockFunc}
+                loading={loading}
+                setStudy={mockFunc}
+                setHasChanged={mockFunc}
+                cache={mockFunc}
+                setUpdateCache={mockFunc}
+                updateCache={mockFunc}
+                setDeleteStudyInProgress={mockFunc}
+            />
+        </Router>
+    );
+    let linkElement = getByText('Edit');
+    expect(linkElement).toBeInTheDocument();
+    // Clicks the Edit button
+    linkElement.click();
+
+    linkElement = getByTestId('study_delete');
+    expect(linkElement).toBeInTheDocument();
+    // Clicks the Edit button
+    linkElement.click();
+
+    linkElement = getByText('Delete study');
+    expect(linkElement).toBeInTheDocument();
+    expect(getByTestId('study_delete').hasAttribute('disabled')).toEqual(false);
+
+    linkElement.click();
+
+    expect(getByText('Delete').closest('button').disabled).toBeTruthy();
+});
+
+test('renders StudyComponent full component with no permission to delete', () => {
+    const history = createMemoryHistory();
+    const { getByText, container, getByTestId } = render(
+        <Router history={history}>
+            <StudyComponentFull
+                study={studyWithNoDelete}
+                newStudy={false}
+                setNewStudy={mockFunc}
+                setLoading={mockFunc}
+                loading={loading}
+                setStudy={mockFunc}
+                setHasChanged={mockFunc}
+                cache={mockFunc}
+                setUpdateCache={mockFunc}
+                updateCache={mockFunc}
+                setDeleteStudyInProgress={mockFunc}
+            />
+        </Router>
+    );
+    let linkElement = getByText('Edit');
+    expect(linkElement).toBeInTheDocument();
+    // Clicks the Edit button
+    linkElement.click();
+
+    linkElement = getByTestId('study_delete_settings');
+    expect(linkElement).toBeInTheDocument();
+    // Clicks the Edit button
+    linkElement.click();
+
+    expect(getByText('Delete study')).toBeInTheDocument();
+
+    expect(getByTestId('study_delete').hasAttribute('disabled')).toEqual(true);
+
+    linkElement = getByTestId('study_delete');
+    linkElement.click();
+});
+
+test('renders StudyComponent full component with sandbox in study', () => {
+    const history = createMemoryHistory();
+    const { getByText, getByTestId } = render(
+        <Router history={history}>
+            <StudyComponentFull
+                study={studyWithSandboxes}
+                newStudy={false}
+                setNewStudy={mockFunc}
+                setLoading={mockFunc}
+                loading={loading}
+                setStudy={mockFunc}
+                setHasChanged={mockFunc}
+                cache={mockFunc}
+                setUpdateCache={mockFunc}
+                updateCache={mockFunc}
+                setDeleteStudyInProgress={mockFunc}
+            />
+        </Router>
+    );
+    let linkElement = getByText('Edit');
+    expect(linkElement).toBeInTheDocument();
+    // Clicks the Edit button
+    linkElement.click();
+
+    linkElement = getByTestId('study_delete_settings');
+    expect(linkElement).toBeInTheDocument();
+    // Clicks the Edit button
+    linkElement.click();
+
+    expect(getByText('Delete study')).toBeInTheDocument();
+
+    expect(getByTestId('study_delete').hasAttribute('disabled')).toEqual(true);
+
+    linkElement = getByTestId('study_delete');
+    linkElement.click();
 });
