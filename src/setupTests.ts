bef3a6736cf69f9d63ceac853ff99c51018268d7
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-04-27 16:00:27 +0200

    fix: issue with integration test and new msal version

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+import { Crypto } from '@peculiar/webcrypto';
+
+global.crypto = new Crypto();
+
+//window.crypto = require('@trust/webcrypto');
