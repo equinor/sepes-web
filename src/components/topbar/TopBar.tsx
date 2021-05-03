4d337537226a609ec737c3d2763f5b9b391fe71c
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-05-03 14:48:54 +0200

    wip

--- src/components/topbar/TopBar.tsx
+++ src/components/topbar/TopBar.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/jsx-fragments, no-shadow */
-import React, { Fragment, useContext, useRef } from 'react';
+import React, { Fragment, useContext } from 'react';
 import styled from 'styled-components';
-import { TopBar, Icon, Tooltip, Button, Menu, Typography } from '@equinor/eds-core-react';
+import { TopBar, Icon, Button, Menu, Typography } from '@equinor/eds-core-react';
 import NavTabs from './NavTabs';
 import { UserConfig } from '../../index';
 import { Link, useHistory } from 'react-router-dom';
 import { account_circle, report_bug, exit_to_app } from '@equinor/eds-icons';
-//import useClickOutside from '../common/customComponents/useClickOutside';
 import { requestChangeLink } from '../common/staticValues/commonLinks';
 
 const { MenuItem } = Menu;
@@ -22,26 +21,6 @@ Icon.add(icons);
 
 const Wrapper = styled.div`
     overflow: auto;
-`;
-
-const Icons = styled.div`
-    display: flex;
-    align-items: center;
-    flex-direction: row-reverse;
-    > * {
-        margin-left: 40px;
-    }
-`;
-
-const LogoutWrapper = styled.div`
-    position: absolute;
-    right: 8px;
-    background-color: #ffffff;
-    padding: 16px;
-    z-index: 99999;
-    border-radius: 4px;
-    box-shadow: 0 0 4px 4px #e7e7e7;
-    text-align: center;
 `;
 
 export const LinkToReport = styled.a`
@@ -71,10 +50,6 @@ const CENTER_CHOICES = {
 const Bar = (props: any) => {
     const leftChoice = 'text+icon';
     const centerChoice = 'tabs';
-    const rightChoice = 'icons';
-    //const [, setIsopen] = useState<boolean>(false);
-    const wrapperRef = useRef(null);
-    // useClickOutside(wrapperRef, setToggle);
     const history = useHistory();
 
     const [state, setState] = React.useState<{
@@ -152,61 +127,12 @@ const Bar = (props: any) => {
             </Menu.Section>
         </>
     );
-    /*
-    const RIGHT_CHOICES = {
-        none: null,
-        text: '',
-        icons: (
-            <Icons>
-                <Tooltip title="User" placement="left">
-                    <Button
-                        id="menuButton"
-                        variant="ghost_icon"
-                        onClick={(e) => (isOpen ? closeMenu() : openMenu(e, 'first'))}
-                    >
-                        <Icon
-                            name="account_circle"
-                            style={{ cursor: 'pointer' }}
-                            size={24}
-                            color="#007079"
-                            className="icon"
-                            title="account"
-                        />
-                    </Button>
-                    <Menu
-                        id="menuButton"
-                        aria-labelledby="menuButton"
-                        open={isOpen}
-                        onClose={closeMenu}
-                        anchorEl={buttonEl}
-                        focus={focus}
-                    >
-                        {optionsTemplate}
-                    </Menu>
-                </Tooltip>
-            </Icons>
-        )
-    };
-    */
-    /*
-    useEffect(() => {
-        document.addEventListener('keydown', listener, false);
-        return () => {
-            document.removeEventListener('keydown', listener, false);
-        };
-    }, []);
-    const listener = (e: any) => {
-        if (e.key === 'Escape') {
-            //setToggle(false);
-        }
-    };
-*/
+
     return (
         <Wrapper>
             <TopBar style={{ position: 'initial' }}>
                 <Header>{LEFT_CHOICES[leftChoice]}</Header>
                 {CENTER_CHOICES[centerChoice]}
-                {/*<Actions>{RIGHT_CHOICES[rightChoice]}</Actions>*/}
                 <TopBar.Actions>
                     <Button
                         id="menuButton"
@@ -234,30 +160,6 @@ const Bar = (props: any) => {
                     </Menu>
                 </TopBar.Actions>
             </TopBar>
-            {/*toggle && (
-                <LogoutWrapper ref={wrapperRef}>
-                    <div>{user.getAllAccounts()[0] && user.getAllAccounts()[0].name}</div>
-                    <Divider color="medium" variant="small" />
-                    <EquinorLink style={{ marginBottom: '8px' }} to="/releasenotes" onClick={onChangelogClick}>
-                        Release Notes
-                    </EquinorLink>
-                    <div style={{ marginTop: '8px' }}>
-                        <a
-                            style={{ marginLeft: '0px' }}
-                            href={requestChangeLink}
-                            onClick={onChangelogClick}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <LinkToReport>Report bug</LinkToReport>
-                        </a>
-                    </div>
-                    <div style={{ marginBottom: '16px' }} />
-                    <EquinorLink to="/" onClick={() => user.logoutRedirect()}>
-                        Log Out
-                    </EquinorLink>
-                </LogoutWrapper>
-            )*/}
         </Wrapper>
     );
 };
