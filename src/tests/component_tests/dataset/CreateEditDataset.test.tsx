7da8b05ec387dd6f62a8e2dc9c9a15d33dff4d46
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-05-04 15:22:17 +0200

    test: mock out package that failed some tests

--- src/tests/component_tests/dataset/CreateEditDataset.test.tsx
+++ src/tests/component_tests/dataset/CreateEditDataset.test.tsx
@@ -1,85 +1,86 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import CreateEditDataset from '../../../components/dataset/CreateEditDataset';
-// import { DatasetObj } from '../../../components/common/interfaces';
-// import { createMemoryHistory } from 'history';
-// import { Router } from 'react-router-dom';
+import React from 'react';
+import { render } from '@testing-library/react';
+import CreateEditDataset from '../../../components/dataset/CreateEditDataset';
+import { DatasetObj } from '../../../components/common/interfaces';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+jest.mock('react-keyed-file-browser', () => jest.fn(() => {}));
 
-// const dataset: DatasetObj = {
-//     name: '',
-//     storageAccountLink: undefined,
-//     permissions: {
-//         deleteDataset: true,
-//         editDataset: true
-//     }
-// };
+const dataset: DatasetObj = {
+    name: '',
+    storageAccountLink: undefined,
+    permissions: {
+        deleteDataset: true,
+        editDataset: true
+    }
+};
 
-// const datasetWithInfo: DatasetObj = {
-//     name: 'test Name',
-//     storageAccountName: 'storage',
-//     classification: 'classification',
-//     location: 'location',
-//     storageAccountLink: undefined,
-//     permissions: {
-//         deleteDataset: true,
-//         editDataset: true
-//     }
-// };
+const datasetWithInfo: DatasetObj = {
+    name: 'test Name',
+    storageAccountName: 'storage',
+    classification: 'classification',
+    location: 'location',
+    storageAccountLink: undefined,
+    permissions: {
+        deleteDataset: true,
+        editDataset: true
+    }
+};
 
-// const mockFunc = (id: string) => {};
+const mockFunc = (id: string) => {};
 
-// test('renders stepbar component without info', () => {
-//     const history = createMemoryHistory();
-//     const { getByTestId } = render(
-//         <Router history={history}>
-//             <CreateEditDataset
-//                 datasetFromDetails={dataset}
-//                 setDatasetFromDetails={mockFunc}
-//                 setShowEditDataset={mockFunc}
-//                 editingDataset
-//             />
-//         </Router>
-//     );
+test('renders stepbar component without info', () => {
+    const history = createMemoryHistory();
+    const { getByTestId } = render(
+        <Router history={history}>
+            <CreateEditDataset
+                datasetFromDetails={dataset}
+                setDatasetFromDetails={mockFunc}
+                setShowEditDataset={mockFunc}
+                editingDataset
+            />
+        </Router>
+    );
 
-//     expect(getByTestId('dataset_save').hasAttribute('disabled')).toEqual(true);
-// });
-// test('renders stepbar component with edit mode and info', () => {
-//     const history = createMemoryHistory();
-//     const { getByTestId, getByText } = render(
-//         <Router history={history}>
-//             <CreateEditDataset
-//                 datasetFromDetails={datasetWithInfo}
-//                 setDatasetFromDetails={mockFunc}
-//                 setShowEditDataset={mockFunc}
-//                 editingDataset
-//             />
-//         </Router>
-//     );
-//     const linkElement = getByText('Edit dataset');
-//     expect(linkElement).toBeInTheDocument();
-//     expect(getByTestId('dataset_save').hasAttribute('disabled')).toEqual(false);
-// });
-
-// test('renders stepbar component with create mode', () => {
-//     const history = createMemoryHistory();
-//     const { getByTestId, getByText } = render(
-//         <Router history={history}>
-//             <CreateEditDataset
-//                 datasetFromDetails={datasetWithInfo}
-//                 setDatasetFromDetails={mockFunc}
-//                 setShowEditDataset={mockFunc}
-//                 editingDataset={false}
-//             />
-//         </Router>
-//     );
-//     const linkElement = getByText('Create dataset');
-//     expect(linkElement).toBeInTheDocument();
-//     expect(getByTestId('dataset_save').hasAttribute('disabled')).toEqual(false);
-// });
-
-function sum(a, b) {
-    return a + b;
-}
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(getByTestId('dataset_save').hasAttribute('disabled')).toEqual(true);
 });
+test('renders stepbar component with edit mode and info', () => {
+    const history = createMemoryHistory();
+    const { getByTestId, getByText } = render(
+        <Router history={history}>
+            <CreateEditDataset
+                datasetFromDetails={datasetWithInfo}
+                setDatasetFromDetails={mockFunc}
+                setShowEditDataset={mockFunc}
+                editingDataset
+            />
+        </Router>
+    );
+    const linkElement = getByText('Edit dataset');
+    expect(linkElement).toBeInTheDocument();
+    expect(getByTestId('dataset_save').hasAttribute('disabled')).toEqual(false);
+});
+
+test('renders stepbar component with create mode', () => {
+    const history = createMemoryHistory();
+    const { getByTestId, getByText } = render(
+        <Router history={history}>
+            <CreateEditDataset
+                datasetFromDetails={datasetWithInfo}
+                setDatasetFromDetails={mockFunc}
+                setShowEditDataset={mockFunc}
+                editingDataset={false}
+            />
+        </Router>
+    );
+    const linkElement = getByText('Create dataset');
+    expect(linkElement).toBeInTheDocument();
+    expect(getByTestId('dataset_save').hasAttribute('disabled')).toEqual(false);
+});
+
+// function sum(a, b) {
+//     return a + b;
+// }
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+// });
