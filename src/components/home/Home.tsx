19b2d890db35663f8df215eddcaeb05761e820f5
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-05-14 08:38:35 +0200

    wip

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -64,6 +64,7 @@ const Home = () => {
                         placement="top"
                     >
                         <Button
+                            disabled={!permissions.canCreateStudy}
                             data-cy="new_study"
                             style={{ width: '336px' }}
                             onClick={() => {
