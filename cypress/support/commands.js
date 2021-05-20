360cd1bb41c500c29d9c37b7c31a7f66b1542420
Hans Kristian Vik Olsen <35257765+HansKristianVikOlsen@users.noreply.github.com>
2021-05-20 16:09:52 +0200

    test(dataset): upload a file (#891)
    * test(dataset): upload a file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,10 +26,13 @@
 
 const _ = Cypress._;
 
+import 'cypress-file-upload';
+
 Cypress.Commands.add('login', (accessType = 'ADMIN') => {
-    // const cyToken = '';
-    // window.localStorage.setItem('cyToken', cyToken);
-    window.localStorage.setItem('cyToken', Cypress.env('cyAccessToken'));
+    const cyToken =
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiJlOTBjYmI2MS04OTZlLTRlYzctYWEzNy0yMzUxMTcwMGUxZWQiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vM2FhNGEyMzUtYjZlMi00OGQ1LTkxOTUtN2ZjZjA1YjQ1OWIwL3YyLjAiLCJpYXQiOjE2MjE1MDM5MTcsIm5iZiI6MTYyMTUwMzkxNywiZXhwIjoxNjIxNTA3ODE3LCJhaW8iOiJBVFFBeS84VEFBQUF6TXd1cy9MbXdMVGUvSG8raml6TXVOWFBpU2dwODliMjNCMEd2b3BRdGNPaS9ESXpzMWJtUXlCd09VRlJxV0tNIiwiYXpwIjoiZTkwY2JiNjEtODk2ZS00ZWM3LWFhMzctMjM1MTE3MDBlMWVkIiwiYXpwYWNyIjoiMCIsImVtYWlsIjoiSEFOT0xAZXF1aW5vci5jb20iLCJuYW1lIjoiSGFucyBLcmlzdGlhbiBWaWsgT2xzZW4iLCJvaWQiOiI3NjIxMDZlZS04YmRhLTQyYjQtOGQ4YS01MzdiMDdiY2FiMDEiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJIQU5PTEBlcXVpbm9yLmNvbSIsInJoIjoiMC5BUUlBTmFLa091SzIxVWlSbFhfUEJiUlpzR0c3RE9sdWljZE9xamNqVVJjQTRlMENBSTguIiwicm9sZXMiOlsiU2VwZXMtU3BvbnNvciIsIlNlcGVzLUFkbWluIiwiU2VwZXMtRW1wbG95ZWUiXSwic2NwIjoiVXNlci5SZWFkIFVzZXIuUmVhZC5BbGwgVXNlci5JbXBlcnNvbmF0aW9uIiwic3ViIjoiOWdGVVZKRlI3WlkwWmxxQ2R5Wk1hZ0pWY3l1WWNfMVJUa2h4QVVCNUsxSSIsInRpZCI6IjNhYTRhMjM1LWI2ZTItNDhkNS05MTk1LTdmY2YwNWI0NTliMCIsInVwbiI6IkhBTk9MQGVxdWlub3IuY29tIiwidXRpIjoiMEtSMVYxX0JJa0tpeEt3elJtRlZBQSIsInZlciI6IjIuMCJ9.jasL5J_3HzlG33yE7LVvYPEOe1KALLUvh7V0aYmZPHL6QZZmNawWVXVGFfq_D28gQMfWsVA0KLkpcZfocnipEjGhM4yKHF7CPKVeoaytCKT_nX9JvtE_dGI0t2BEeoVyjQrYAJ5CMsC-snSnaiwoxUEya8eCZ1ndGu3cvHka3BkV4o5Iu7pkxgWPGomshgkBjmbgvlfqumNntOyfmvASZljpbeKA9_MvfiG3u_-e8s1VlxItspviPGw4S3ZUHDiYVKWq43vj4VMWV962bG_mF_8L2ExVsOqd8a030aLK-dwb4HWO6FsZlenVEy8kOuPpq9NWKQ-BYtHqZ43BlqdeHQ';
+    window.localStorage.setItem('cyToken', cyToken);
+    // window.localStorage.setItem('cyToken', Cypress.env('cyAccessToken'));
 });
 
 Cypress.Commands.add('createStudy', (studyName) => {
@@ -44,9 +47,6 @@ Cypress.Commands.add('createStudy', (studyName) => {
 
     cy.get('[data-cy=create_study]').click({ force: true });
     cy.waitForStudyToLoad();
-    // cy.intercept('/api/studies/*').as('getStudy');
-    // cy.wait(2000);
-    // cy.wait('@getStudy');
 });
 
 Cypress.Commands.add('createDataset', () => {
