e27992f8f266e3c978eed79b634444322fc0671c
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-05-25 15:46:11 +0200

    wip

--- cypress/integration/quickRunningTests/createVm/createVm.spec.js
+++ cypress/integration/quickRunningTests/createVm/createVm.spec.js
@@ -36,42 +36,15 @@ describe('Create vm', () => {
     const sandboxName = 'Cypress ' + Cypress._.random(0, 1e6);
 
     it('fills out sanbox information', () => {
-        cy.get('[data-cy=sandbox_name]').type(sandboxName);
-        cy.get('[data-cy=sandbox_region]').click({ force: true });
-        cy.contains('Norway East').click({ force: true });
-        cy.get('[data-cy=create_actual_sandbox]').click({ force: true });
+        cy.createSandbox(sandboxName);
     });
 
     it('fills out vm information', () => {
-        // cy.wait(5000);
-        cy.waitForSandboxToLoad();
-        cy.get('[data-cy=vm_name]').type('cy name');
-        cy.get('[data-cy=vm_operatingSystem]').click({ force: true });
-        cy.contains('Windows Server 2019 Datacenter').click({ force: true });
-        cy.get('[data-cy=vm_username]').type('cy username');
-        cy.get('[data-cy=vm_password]').type('Cypassword123!!');
-        cy.get('[data-cy=vm_size]').click({ force: true });
-        cy.contains('Standard_F1').click({ force: true });
-
-        cy.get('[data-cy=vm_dataDisks]').click({ force: true });
-        cy.contains('64 GB').click({ force: true });
-        // cy.wait(3000);
-        cy.get('[data-cy=create_vm]').click();
-        cy.waitForVirtualMachineToBeCreated();
-        // cy.wait(3000);
+        cy.createVm();
     });
 
     it('creates VM rules', () => {
-        cy.wait(3000);
-        cy.get('[data-cy=vm_add_rule]').click();
-        // cy.wait(1000);
-        cy.get('[data-cy=vm_rule_description]').type('cy rule description');
-        cy.get('[data-cy=vm_rule_ip]').type('192.168.1.1');
-
-        cy.get('[data-cy="vm_rule_protocol"]').click({ force: true });
-        cy.contains('HTTP').click({ force: true });
-
-        cy.get('[data-cy=vm_rule_save]').click({ force: true });
+        cy.createVmRules();
     });
 
     it('add data set to sandbox', () => {
@@ -80,18 +53,11 @@ describe('Create vm', () => {
     });
 
     it('delete vm', () => {
-        cy.get('[data-cy=vm_more_actions]').click({ force: true });
-        cy.get('[data-cy="vm_delete"]').click({ force: true });
-        cy.get('[data-cy="delete_resource"]').type('vm-cytitle-' + sandboxName + '-cyname');
-        cy.get('[data-cy=delete_resource_delete]').click({ force: true });
-        cy.wait(1000);
+        cy.deleteVm(sandboxName);
     });
 
     it('delete sandbox', () => {
-        cy.get('[data-cy=sandbox_more_options]').click({ force: true });
-        cy.get('[data-cy=sandbox_delete]').click({ force: true });
-        cy.get('[data-cy="delete_resource"]').type(sandboxName);
-        cy.get('[data-cy=delete_resource_delete]').click({ force: true });
+        cy.deleteSandbox(sandboxName);
     });
     it('delete study', () => {
         cy.deleteStudy(studyName);
