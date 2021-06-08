5e4a18d1a82afde51f8c90fd46ca123cae610b7e
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-06-08 13:55:21 +0200

    wip

--- jest.config.js
+++ jest.config.js
@@ -49,7 +49,7 @@ module.exports = {
     //   "\\\\node_modules\\\\"
     // ],
     // A list of reporter names that Jest uses when writing coverage reports
-    coverageReporters: ['json', 'text', 'lcov', 'clover'],
+    coverageReporters: ['text-summary'],
     // An object that configures minimum threshold enforcement for coverage results
     // coverageThreshold: null,
     // A path to a custom dependency extractor
