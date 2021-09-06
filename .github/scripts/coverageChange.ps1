c2e8d2c4cfe8df667edee23190ae4ca0ab4a53da
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-09-06 13:04:09 +0200

    wip

---
+++ .github/scripts/coverageChange.ps1
@@ -0,0 +1,12 @@
+$currentCoverage=$args[0]
+$previousCoverage=$args[1]
+
+if ($currentCoverage == $previousCoverage) {
+    return "Coverage has not changed (" + $currentCoverage + "%)";
+}
+if ($currentCoverage -gt $previousCoverage) {
+    return 'Coverage has increased by (' + ($currentCoverage - $previousCoverage) + "%)";
+}
+else {
+    return 'Coverage has decreased by (' +( $previousCoverage - $currentCoverage) + "%)";
+}
