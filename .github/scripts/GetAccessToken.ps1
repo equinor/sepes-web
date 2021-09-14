b5a595bda72930de215f8bca70063f61de8329b5
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-09-14 11:17:05 +0200

    wip

--- .github/scripts/GetAccessToken.ps1
+++ .github/scripts/GetAccessToken.ps1
@@ -2,21 +2,20 @@ param (
     [string]$clientId = $null,
     [string]$tenantId = $null,
     [string]$clientSecret = $null,
-    [string]$scope = $null,
-    [boolean]$verbose = $false
+    [string]$scope = $null
 )
 
 $body = @{client_id=$clientId;client_secret=$clientSecret;grant_type="client_credentials";scope="${scope}/.default";}
 $oAuthReq = Invoke-RestMethod -Method Post -Uri https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token -Body $body
 $accessToken = $oAuthReq.access_token
-
+$verbose = $true;
 
 if ($accessToken.Length -gt 20) {
     if ($verbose -eq $true) {
-        Write-Host "$(CurrentLineStart)Access token ...$($accessToken.substring(20, 20))... was received" -ForegroundColor Green
+        Write-Host "Access token was received" -ForegroundColor Green
     }
 } else {
-    Write-Host "$(CurrentLineStart)Access token could not be retrieved" -ForegroundColor Red
+    Write-Host "Access token could not be retrieved" -ForegroundColor Red
     exit
 }
 
