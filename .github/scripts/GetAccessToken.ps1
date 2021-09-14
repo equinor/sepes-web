dbbc30d6321104c8e12ff50495b888599f2de825
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-09-14 13:55:51 +0200

    wip

--- .github/scripts/GetAccessToken.ps1
+++ .github/scripts/GetAccessToken.ps1
@@ -17,7 +17,7 @@ $clientSecret = $args[2];
 $scope = $args[3];
 
 
-$body = @{client_id=$clientId;client_secret="$clientSecret";grant_type="client_credentials";scope="${scope}/.default";}
+$body = @{client_id=$clientId;client_secret=$clientSecret;grant_type="client_credentials";scope="${scope}/.default";}
 $oAuthReq = Invoke-RestMethod -Method Post -Uri https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token -Body $body
 $accessToken = $oAuthReq.access_token
 # $verbose = $true;
