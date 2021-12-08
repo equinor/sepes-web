247d56bf6faf182518cefae192b70f88c4a71682
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-12-08 14:29:59 +0100

    docs: clear viewport for video

--- cypress/integration/documentationVideos/createSandboxAndVirtualMachine.spec.js
+++ cypress/integration/documentationVideos/createSandboxAndVirtualMachine.spec.js
@@ -3,7 +3,7 @@ describe('Create sandbox and virtual machine', () => {
     const studyName = 'Study for documentation';
     before(() => {
         cy.login();
-        // cy.clearViewport();
+        cy.clearViewport();
     });
 
     beforeEach(() => {
