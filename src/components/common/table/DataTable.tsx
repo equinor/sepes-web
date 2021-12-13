fca2919ede23382a189d29106d27bc2067b6cf18
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-12-13 10:56:05 +0100

    feat: add sorting and pagination to participant table
    If you add many participants to a study, the page would get longer and longer. To resolve this, pagination is added
    
    Closes #1387

---
+++ src/components/common/table/DataTable.tsx
@@ -0,0 +1,359 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import { Table, Icon } from '@equinor/eds-core-react';
+import { chevron_down, chevron_up, unfold_more } from '@equinor/eds-icons';
+import { Pagination } from './Pagination';
+import Cookies from 'js-cookie';
+
+Icon.add({ chevron_down, chevron_up, unfold_more });
+
+let prevPage;
+let prevPerPage;
+
+export const DataTable = (props) => {
+    const {
+        data,
+        useExternalPagingAndSorting,
+        setExternalPerPage,
+        setExternalPageIndex,
+        setExternalSortBy,
+        externalTotalItems,
+        columns,
+        cookiePrefix,
+        loading,
+        abortAndSetNewController
+    } = props;
+    const columnsCookie = cookiePrefix + 'Columns';
+    const perPageCookie = cookiePrefix + 'PerPage';
+    const pageNumberCookie = cookiePrefix + 'PageNumber';
+    const _columns = Cookies.get(columnsCookie);
+    const columnsToUse = (columnsCookie && _columns && JSON.parse(_columns)) || columns;
+    const [state, setState] = useState<{
+        columns: any;
+        cellValues?: string[][];
+    }>({ columns: columnsToUse });
+    const pageNumberFromCookie = Number(Cookies.get(pageNumberCookie));
+    const [perPage, setPerPage] = useState(Number(Cookies.get(perPageCookie)) || 10);
+    const [currentPageIndex, setCurrentPageIndex] = useState(pageNumberFromCookie || 1);
+    const [viewableData, setViewableData] = useState([]);
+    const [sortedData, setSortedData] = useState(data);
+    const [updateTable, setUpdateTable] = useState(true);
+    // const [showLoading, setShowLoading] = useState(false);
+    const [columnHover, setColumnHover] = useState('');
+    const [itemNumbers, setItemNumbers] = useState({
+        start: 1,
+        end: 10
+    });
+
+    const [hidePagination, setHidePagination] = useState(false);
+    console.log(columns);
+    useEffect(() => {
+        if (props.tableToPageOne) {
+            props.setTableToPageOne(false);
+            props.setFetchReports(true);
+            forcePaginationToPage(1);
+        }
+    }, [props.tableToPageOne]);
+
+    const forcePaginationToPage = (_pageNumber) => {
+        setExternalPageIndex(_pageNumber - 1);
+        setCurrentPageIndex(_pageNumber);
+        setHidePagination(true);
+        setTimeout(() => {
+            setHidePagination(false);
+        }, 10);
+        Cookies.set(pageNumberCookie, _pageNumber, { expires: 365 });
+    };
+
+    useEffect(() => {
+        if (useExternalPagingAndSorting && props.userHasAbortedRequest) {
+            let newPerPage = prevPerPage || 10;
+
+            if (prevPerPage < 10) {
+                newPerPage = 10;
+            }
+            if (!props.tableToPageOne && newPerPage === perPage) {
+                forcePaginationToPage(prevPage < 1 ? 1 : prevPage);
+            } else {
+                setPerPage(newPerPage);
+                setExternalPerPage(newPerPage);
+                Cookies.set(perPageCookie, newPerPage, { expires: 365 });
+                if (currentPageIndex !== prevPage) {
+                    forcePaginationToPage(prevPage < 1 ? 1 : prevPage);
+                }
+            }
+            props.setUserHasAbortedRequest(false);
+        }
+    }, [props.userHasAbortedRequest, props.tableToPageOne]);
+
+    useEffect(() => {
+        if (!props.isFetchingAnyRequest && useExternalPagingAndSorting) {
+            prevPage = currentPageIndex;
+            prevPerPage = perPage;
+        }
+    }, [props.isFetchingAnyRequest]);
+
+    const setAsSelected = () => {
+        const _perPage = Number(Cookies.get(perPageCookie));
+        if (!Number.isNaN(_perPage)) {
+            setPerPage(_perPage);
+        }
+        if (useExternalPagingAndSorting) {
+            if (_columns) {
+                const sortByColumn = JSON.parse(_columns).find((col) => col.isSorted);
+                setExternalSortBy(sortByColumn);
+            }
+        }
+    };
+
+    const changePage = (_pageIndex) => {
+        if (_pageIndex === currentPageIndex) {
+            return;
+        }
+        setCurrentPageIndex(_pageIndex);
+        if (useExternalPagingAndSorting) {
+            if (!props.isFetchingAnyRequest) {
+                prevPage = currentPageIndex;
+            }
+            //we can assume that consumers of this component use 0-based page indexing
+
+            setExternalPageIndex(_pageIndex - 1);
+            props.setFetchReports(true);
+            abortAndSetNewController();
+        }
+        Cookies.set(pageNumberCookie, _pageIndex, { expires: 365 });
+    };
+
+    const changePageSize = (_perPage) => {
+        if (_perPage === perPage) {
+            return;
+        }
+        setPerPage(_perPage);
+        setCurrentPageIndex(0);
+
+        if (useExternalPagingAndSorting) {
+            if (!props.isFetchingAnyRequest) {
+                prevPerPage = perPage;
+            }
+
+            abortAndSetNewController();
+            setExternalPageIndex(0);
+            setExternalPerPage(_perPage);
+            props.setFetchReports(true);
+        }
+        Cookies.set(pageNumberCookie, 1, { expires: 365 });
+        Cookies.set(perPageCookie, _perPage, { expires: 365 });
+    };
+
+    useEffect(() => {
+        setAsSelected();
+    }, []);
+
+    const recalculateItemNumbers = () => {
+        const firstItemInPageIndex = currentPageIndex === 1 ? 1 : perPage * (currentPageIndex - 1) + 1;
+        const totalItems = useExternalPagingAndSorting ? externalTotalItems : data.length;
+
+        setItemNumbers({
+            start: firstItemInPageIndex,
+            end: Math.min(firstItemInPageIndex + perPage - 1, totalItems)
+        });
+    };
+
+    useEffect(() => {
+        setUpdateTable(true);
+        if (!useExternalPagingAndSorting) {
+            setViewableData(returnSlicedData(sortedData));
+        } else {
+            recalculateItemNumbers();
+        }
+    }, [currentPageIndex, perPage, data]);
+
+    // useEffect(() => {
+    //     setCurrentPageIndex(1);
+    //     if (useExternalPagingAndSorting) {
+    //         //we can assume that consumers of this component use 0-based page indexing
+    //         setExternalPageIndex(currentPageIndex - 1);
+    //     }
+    // }, [perPage]);
+
+    const returnSlicedData = (_data) => {
+        const firstItemInPageIndex = currentPageIndex === 1 ? 0 : perPage * (currentPageIndex - 1);
+        recalculateItemNumbers();
+        return _data.slice(firstItemInPageIndex, firstItemInPageIndex + perPage);
+    };
+
+    const onSortClick = (sortCol) => {
+        setUpdateTable(true);
+        const updateColumns =
+            state.columns &&
+            state.columns.map((col) => {
+                if (sortCol.accessor === col.accessor) {
+                    const isSorted = true;
+                    let sortDirection = 'none';
+                    switch (sortCol.sortDirection) {
+                        case 'ascending':
+                            sortDirection = 'descending';
+                            break;
+                        default:
+                            sortDirection = 'ascending';
+                            break;
+                    }
+                    //Use this if you want "none" to be a third state of the sorting
+                    // case 'descending':
+                    //     isSorted = false;
+                    //     sortDirection = 'none';
+                    //     break;
+
+                    return {
+                        ...sortCol,
+                        isSorted,
+                        sortDirection
+                    };
+                }
+                return {
+                    ...col,
+                    isSorted: false,
+                    sortDirection: col.sortDirection ? 'none' : undefined
+                };
+            });
+        Cookies.set(columnsCookie, updateColumns);
+        setState({ ...state, columns: updateColumns });
+        const updatedColumn = updateColumns.find((col) => col.accessor === sortCol.accessor);
+        if (useExternalPagingAndSorting) {
+            setExternalSortBy(updatedColumn);
+            abortAndSetNewController();
+        }
+    };
+
+    const sortData = useCallback(
+        (_viewableData) =>
+            _viewableData.sort((right, left) => {
+                const sortedCol = state.columns && state.columns.find((col) => col.isSorted);
+                if (!sortedCol) {
+                    return 1;
+                }
+                const { sortDirection, accessor } = sortedCol;
+                let leftValue;
+                let rightValue;
+                //TODO: see if this can be improved. This handles accessors with nested properties
+                const accessorArray = accessor.split('.');
+                if (accessor.includes('.')) {
+                    leftValue = left[accessorArray[0]][accessorArray[1]]
+                        ? left[accessorArray[0]][accessorArray[1]].toString().toLowerCase()
+                        : '';
+                    rightValue = right[accessorArray[0]][accessorArray[1]]
+                        ? right[accessorArray[0]][accessorArray[1]].toString().toLowerCase()
+                        : '';
+                } else {
+                    leftValue = left[accessor] ? left[accessor].toString().toLowerCase() : '';
+                    rightValue = right[accessor] ? right[accessor].toString().toLowerCase() : '';
+                }
+
+                if (sortDirection === 'ascending') {
+                    return leftValue < rightValue ? 1 : -1;
+                }
+                if (sortDirection === 'descending') {
+                    return leftValue > rightValue ? 1 : -1;
+                }
+                return {};
+            }),
+        [state.columns, data, props.listItems, viewableData]
+    );
+
+    useEffect(() => {
+        if (columns) {
+            if (!useExternalPagingAndSorting) {
+                if (updateTable) {
+                    const sorted = sortData(data);
+                    setSortedData(data);
+                    setState((prevState) => ({ ...prevState, cellValues: returnSlicedData(sorted) }));
+                    setUpdateTable(false);
+                }
+            } else {
+                setSortedData(data);
+                recalculateItemNumbers();
+                setState((prevState) => ({ ...prevState, cellValues: data }));
+            }
+        }
+    }, [state.columns, setState, sortData, data]);
+
+    const getColumnStyle = (col) => {
+        return {
+            ...col.style,
+            background: !col.isSortable ? '#F7F7F7' : '',
+            cursor: !col.isSortable ? 'default' : ''
+        };
+    };
+
+    return (
+        <>
+            <Table style={{ width: '100%' }}>
+                <Table.Head>
+                    <Table.Row>
+                        {state.columns.map((col) => (
+                            <Table.Cell
+                                scope="col"
+                                sort={col.sortDirection}
+                                key={'head-' + col.accessor + col.key}
+                                onClick={col.sortDirection && col.isSortable ? () => onSortClick(col) : undefined}
+                                style={getColumnStyle(col)}
+                                data-cy={col.datacy}
+                                onMouseEnter={() => setColumnHover(col.key)}
+                                onMouseLeave={() => setColumnHover('')}
+                            >
+                                {col.name}
+                                {col.isSortable && col.sortDirection === 'none' ? (
+                                    <Icon
+                                        style={{ fill: col.key !== columnHover ? 'lightgrey' : '' }}
+                                        name="unfold_more"
+                                    />
+                                ) : (
+                                    col.isSortable && (
+                                        <Icon
+                                            name={
+                                                col.sortDirection === 'descending'
+                                                    ? 'chevron_down'
+                                                    : col.sortDirection === 'ascending'
+                                                    ? 'chevron_up'
+                                                    : 'unfold_more'
+                                            }
+                                        />
+                                    )
+                                )}
+                            </Table.Cell>
+                        ))}
+                    </Table.Row>
+                </Table.Head>
+                <Table.Body style={{ opacity: loading ? '0.5' : '1' }}>
+                    {state.cellValues && state.cellValues.length > 0 ? (
+                        state.cellValues?.slice(0, perPage).map((row) => props.listItems(row))
+                    ) : (
+                        <Table.Row>
+                            {columns.map((a, b) => {
+                                if (b === 0) {
+                                    return <Table.Cell key={1}>No items in table</Table.Cell>;
+                                }
+                                return <Table.Cell key={a.key} />;
+                            })}
+                        </Table.Row>
+                    )}
+                </Table.Body>
+            </Table>
+            {!hidePagination && (
+                <Pagination
+                    totalItems={useExternalPagingAndSorting ? externalTotalItems : data.length}
+                    initialItemsPerPage={perPage}
+                    initialPage={currentPageIndex}
+                    onChangePage={(pageIndex) => {
+                        changePage(pageIndex);
+                    }}
+                    onChangeItemsPerPage={(itemsPerPage) => {
+                        changePageSize(itemsPerPage);
+                    }}
+                    itemNumbers={itemNumbers}
+                />
+            )}
+        </>
+    );
+};
+
+export default DataTable;
--- src/components/studyDetails/Tables/ParticipantTable.tsx
+++ src/components/studyDetails/Tables/ParticipantTable.tsx
@@ -1,12 +1,51 @@
-/* eslint-disable react/jsx-curly-brace-presence */
+/* eslint-disable react/jsx-curly-brace-presence, consistent-return */
 import React from 'react';
 import { Table, Icon, Button } from '@equinor/eds-core-react';
 import { ParticipantObj, StudyPermissions } from '../../common/interfaces';
 import { studyOwner } from '../../common/staticValues/Roles';
 import useWindowDimensions from '../../common/hooks/useWindowDimensions';
+import DataTable from '../../common/table/DataTable';
 import '../../../styles/Table.scss';
 
-const { Body, Row, Cell, Head } = Table;
+const { Row, Cell } = Table;
+
+const columns = [
+    {
+        key: 'name',
+        style: { textAlign: 'left' },
+        name: 'Entity name',
+        accessor: 'fullName',
+        sortDirection: 'ascending',
+        isSortable: true,
+        isSorted: true
+    },
+    {
+        key: 'code',
+        name: 'E-mail',
+        style: { textAlign: 'left' },
+        accessor: 'emailAddress',
+        sortDirection: 'none',
+        isSortable: true
+    },
+    {
+        key: 'role',
+        name: 'Role',
+        maxWidth: 250,
+        accessor: 'role',
+        sortDirection: 'none',
+        style: { textAlign: 'left' },
+        isSortable: true
+    },
+    {
+        key: '',
+        name: '',
+        maxWidth: 250,
+        accessor: 'entityAccessGroup.code',
+        sortDirection: 'none',
+        isSortable: false,
+        style: { width: '10px' }
+    }
+];
 
 type DatasetsTableProps = {
     participants: any;
@@ -17,51 +56,92 @@ type DatasetsTableProps = {
 
 const DatasetsTable: React.FC<DatasetsTableProps> = ({ participants, removeParticipant, editMode, permissions }) => {
     const { width } = useWindowDimensions();
+    console.log(editMode);
+
+    const returnListOfItems = (participant: ParticipantObj) => {
+        if (participant === undefined) {
+            return;
+        }
+        return (
+            <Row key={participant.userId + participant.role} id="tableRowNoPointerNoColor">
+                <Cell>{participant.fullName}</Cell>
+                {editMode && width > 800 && <Cell align="right">{participant.emailAddress}</Cell>}
+                <Cell align="right">{participant.role}</Cell>
+                {editMode && participant.role !== studyOwner ? (
+                    <Cell align="right">
+                        {permissions?.addRemoveParticipant && (
+                            <Button
+                                variant="ghost_icon"
+                                onClick={() => removeParticipant(participant)}
+                                style={{ color: '#3D3D3D' }}
+                                data-cy="study_remove_participant"
+                            >
+                                <Icon name="close" style={{ cursor: 'pointer' }} size={24} />
+                            </Button>
+                        )}
+                    </Cell>
+                ) : (
+                    editMode && <Cell>{''}</Cell>
+                )}
+            </Row>
+        );
+    };
+
     return (
         <div>
-            <Table style={{ width: '100%', marginBottom: '24px' }}>
-                <Head>
-                    <Row>
-                        <Cell scope="col">Name</Cell>
-                        {editMode && width > 800 && <Cell scope="col">E-mail</Cell>}
-                        <Cell scope="col">Role</Cell>
-                        {editMode && (
-                            <Cell style={{ width: '10px' }} scope="col">
-                                {''}
-                            </Cell>
-                        )}
-                    </Row>
-                </Head>
-                <Body>
-                    {participants &&
-                        participants.length > 0 &&
-                        participants.map((participant: ParticipantObj) => (
-                            <Row key={participant.userId + participant.role} id="tableRowNoPointerNoColor">
-                                <Cell>{participant.fullName}</Cell>
-                                {editMode && width > 800 && <Cell align="right">{participant.emailAddress}</Cell>}
-                                <Cell align="right">{participant.role}</Cell>
-                                {editMode && participant.role !== studyOwner ? (
-                                    <Cell align="right">
-                                        {permissions?.addRemoveParticipant && (
-                                            <Button
-                                                variant="ghost_icon"
-                                                onClick={() => removeParticipant(participant)}
-                                                style={{ color: '#3D3D3D' }}
-                                                data-cy="study_remove_participant"
-                                            >
-                                                <Icon name="close" style={{ cursor: 'pointer' }} size={24} />
-                                            </Button>
-                                        )}
-                                    </Cell>
-                                ) : (
-                                    editMode && <Cell>{''}</Cell>
-                                )}
-                            </Row>
-                        ))}
-                </Body>
-            </Table>
+            <DataTable
+                columns={editMode ? columns : columns.slice(0, 2)}
+                data={participants}
+                listItems={returnListOfItems}
+                cookiePrefix={'adminEntities-editMode' + editMode}
+            />
         </div>
     );
+    // return (
+    //     <div>
+    //         <Table style={{ width: '100%', marginBottom: '24px' }}>
+    //             <Head>
+    //                 <Row>
+    //                     <Cell scope="col">Name</Cell>
+    //                     {editMode && width > 800 && <Cell scope="col">E-mail</Cell>}
+    //                     <Cell scope="col">Role</Cell>
+    //                     {editMode && (
+    //                         <Cell style={{ width: '10px' }} scope="col">
+    //                             {''}
+    //                         </Cell>
+    //                     )}
+    //                 </Row>
+    //             </Head>
+    //             <Body>
+    //                 {participants &&
+    //                     participants.length > 0 &&
+    //                     participants.map((participant: ParticipantObj) => (
+    //                         <Row key={participant.userId + participant.role} id="tableRowNoPointerNoColor">
+    //                             <Cell>{participant.fullName}</Cell>
+    //                             {editMode && width > 800 && <Cell align="right">{participant.emailAddress}</Cell>}
+    //                             <Cell align="right">{participant.role}</Cell>
+    //                             {editMode && participant.role !== studyOwner ? (
+    //                                 <Cell align="right">
+    //                                     {permissions?.addRemoveParticipant && (
+    //                                         <Button
+    //                                             variant="ghost_icon"
+    //                                             onClick={() => removeParticipant(participant)}
+    //                                             style={{ color: '#3D3D3D' }}
+    //                                             data-cy="study_remove_participant"
+    //                                         >
+    //                                             <Icon name="close" style={{ cursor: 'pointer' }} size={24} />
+    //                                         </Button>
+    //                                     )}
+    //                                 </Cell>
+    //                             ) : (
+    //                                 editMode && <Cell>{''}</Cell>
+    //                             )}
+    //                         </Row>
+    //                     ))}
+    //             </Body>
+    //         </Table>
+    //     </div>
+    // );
 };
 
 export default DatasetsTable;
