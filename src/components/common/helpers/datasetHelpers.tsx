293980b8da0b02564c3cbc2cfffbef7d1a91847a
Hans Kristian Vik Olsen <hanol@equinor.com>
2021-12-14 14:34:30 +0100

    fix: validate data set names
    Closes #1384

--- src/components/common/helpers/datasetHelpers.tsx
+++ src/components/common/helpers/datasetHelpers.tsx
@@ -1,8 +1,8 @@
 import { DatasetObj } from '../interfaces';
-import { checkIfInputIsNumberWihoutCharacters, round } from './helpers';
+import { checkIfInputIsNumberWihoutCharacters, round, validateResourceName } from './helpers';
 
 export const checkForInputErrors = (dataset: DatasetObj) => {
-    if (!dataset?.name?.length || !dataset?.classification?.length || !dataset?.location?.length) {
+    if (!validateResourceName(dataset?.name) || !dataset?.classification?.length || !dataset?.location?.length) {
         return true;
     }
     if (dataset?.dataId && !checkIfInputIsNumberWihoutCharacters(dataset?.dataId.toString())) {
