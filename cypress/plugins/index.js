6adc28e51c386af6d9d1d5e02bf40f9c3c9cf91b
Hans Kristian Vik Olsen <hanol@equinor.com>
2022-01-03 11:24:06 +0100

    wip

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,9 +16,9 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  require('cypress-plugin-retries/lib/plugin')(on)
-  require('@cypress/code-coverage/task')(on, config)
-  return config
-}
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+    require('cypress-plugin-retries/lib/plugin')(on);
+    require('@cypress/code-coverage/task')(on, config);
+    return config;
+};
