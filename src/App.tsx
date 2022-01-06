9e3b27e666c998b964e37387e2accda4df5e3329
Hans Kristian Vik Olsen <hanol@equinor.com>
2022-01-06 11:00:02 +0100

    refactor: quick fix deployment 4

--- src/App.tsx
+++ src/App.tsx
@@ -21,16 +21,17 @@ export const UpdateCache = React.createContext({} as IContextProps);
 
 const App = () => {
     const [updateCache, setUpdateCache] = useState();
-    const appInsights = new ApplicationInsights({
-        config: {
-            instrumentationKey: process.env.REACT_APP_INSTRUMENTATION_KEY
-        }
-    });
-    appInsights.loadAppInsights();
+    // const appInsights = new ApplicationInsights({
+    //     config: {
+    //         instrumentationKey: process.env.REACT_APP_INSTRUMENTATION_KEY
+    //     }
+    // });
+    // appInsights.loadAppInsights();
+    // <Layout appInsights={appInsights}>
 
     return (
         <Router>
-            <Layout appInsights={appInsights}>
+            <Layout>
                 <UpdateCache.Provider value={{ updateCache, setUpdateCache }}>
                     <Switch>
                         <Route exact path="/" component={Home} />
