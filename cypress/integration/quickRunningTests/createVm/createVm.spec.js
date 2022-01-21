80e89ea22d077651e64fedb0eb78628ae15dc892
Hans Kristian Vik Olsen <hanol@equinor.com>
2022-01-21 10:17:35 +0100

    test: fix cypress tests

--- cypress/integration/quickRunningTests/createVm/createVm.spec.js
+++ cypress/integration/quickRunningTests/createVm/createVm.spec.js
@@ -12,9 +12,9 @@ describe('Create vm', () => {
     });
 
     it('clicks on data sets tab', () => {
-        cy.waitForStudyToLoad();
-        cy.wait(2000);
-        cy.get('[data-cy=datasets_tab]').click();
+        // cy.waitForStudyToLoad();
+        // cy.get('[data-cy=datasets_tab]').click();
+        cy.switchToDatasetsTab();
     });
 
     it('clicks add study specific dataset', () => {
