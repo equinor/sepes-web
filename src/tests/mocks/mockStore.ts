433ab63e81ee4a82b3055a7eba5cf5dbefafa271
Hans Kristian Vik Olsen <35257765+HansKristianVikOlsen@users.noreply.github.com>
2022-01-28 09:52:22 +0100

    refactor: add data set to redux state management (#1543)
    * refactor: add data set to redux state management

--- src/tests/mocks/mockStore.ts
+++ src/tests/mocks/mockStore.ts
@@ -1,6 +1,7 @@
 import sandboxesReducer from '../../store/sandboxes/sandboxesSlice';
 import resourcesReducer from '../../store/resources/resourcesSlice';
 import studiesReducer from '../../store/studies/studiesSlice';
+import datasetsReducer from '../../store/datasets/datasetsSlice';
 import { configureStore } from '@reduxjs/toolkit';
 
 export const mockStore = (state: any) => {
@@ -8,7 +9,8 @@ export const mockStore = (state: any) => {
         reducer: {
             sandboxes: sandboxesReducer,
             resources: resourcesReducer,
-            studies: studiesReducer
+            studies: studiesReducer,
+            datasets: datasetsReducer
         },
         preloadedState: state
     });
