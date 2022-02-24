096933b8b4679d2b66287493648df074e063e353
Bonyfus Martin <bonyfus@easee.no>
2022-02-24 11:14:26 +0100

    test commit

--- src/tests/mocks/mockStore.ts
+++ src/tests/mocks/mockStore.ts
@@ -2,6 +2,8 @@ import sandboxesReducer from '../../store/sandboxes/sandboxesSlice';
 import resourcesReducer from '../../store/resources/resourcesSlice';
 import studiesReducer from '../../store/studies/studiesSlice';
 import datasetsReducer from '../../store/datasets/datasetsSlice';
+import userSettingsReducer from '../../store/usersettings/userSettingsSlice';
+import screenLoadingReducer from '../../store/screenloading/screenLoadingSlice';
 import { configureStore } from '@reduxjs/toolkit';
 
 export const mockStore = (state: any) => {
@@ -10,7 +12,9 @@ export const mockStore = (state: any) => {
             sandboxes: sandboxesReducer,
             resources: resourcesReducer,
             studies: studiesReducer,
-            datasets: datasetsReducer
+            datasets: datasetsReducer,
+            userSettings: userSettingsReducer,
+            screenLoading: screenLoadingReducer
         },
         preloadedState: state
     });
