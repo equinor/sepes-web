5c5879c872ef4a6cd0c0dcc4d7bba2e947a362d4
Bonyfus Martin <bonm@equinor.com>
2022-05-30 14:50:10 +0200

    ci: return config
    return config

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -19,4 +19,6 @@
  module.exports = (_on: any, _config: any) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
+
+    return _config;
 };
