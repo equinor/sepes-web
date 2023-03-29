0a25bfb90fe238393458944f031af73bcbf60c31
Jone-Torgersen <jone.torgersen@bouvet.no>
2023-03-29 15:46:18 +0200

    chore: remove sepes code

--- src/components/common/upload/DropzoneFile.tsx
+++ src/components/common/upload/DropzoneFile.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useDropzone } from 'react-dropzone';
 import styled from 'styled-components';
-import { EquinorIcon } from '../StyledComponents';
 
 const ChooseImgdiv = styled.div<{ dragActive: boolean }>`
     height: 128px;
@@ -30,46 +29,7 @@ const Dropzone: React.FC<DropzoneProps> = ({ onDrop, accept, disabled, loading }
         accept
     });
 
-    return (
-        <ChooseImgdiv
-            {...getRootProps()}
-            style={{
-                opacity: disabled ? 0.5 : 1,
-                pointerEvents: disabled ? 'none' : 'initial'
-            }}
-            dragActive={isDragActive}
-            data-cy="file_upload_check"
-        >
-            <input className="dropzone-input" {...getInputProps()} data-cy="file_upload" />
-            <div className="text-center">
-                {isDragActive && !disabled ? (
-                    <div className="dropzone-content" style={{ textAlign: 'center' }}>
-                        {EquinorIcon('cloud_upload', '#007079', 32)}
-                        <div>Drop here</div>
-                    </div>
-                ) : (
-                    <div className="dropzone-content" style={{ textAlign: 'center' }}>
-                        {EquinorIcon('cloud_upload', '#007079', 32)}
-                        <div>
-                            {!loading ? (
-                                <>
-                                    {!disabled ? (
-                                        <>
-                                            Drop files or <span style={{ color: '#007079' }}>browse</span> to upload
-                                        </>
-                                    ) : (
-                                        'You do not have access to upload'
-                                    )}
-                                </>
-                            ) : (
-                                'File upload will be available when the storage account is ready'
-                            )}
-                        </div>
-                    </div>
-                )}
-            </div>
-        </ChooseImgdiv>
-    );
+    return <div />;
 };
 
 Dropzone.defaultProps = {
--- src/index.tsx
+++ src/index.tsx
@@ -4,68 +4,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { SignInSilentRedirect } from './auth/AuthFunctions';
-import { myMSALObj } from './auth/AuthConfig';
-import { getPermissions } from './services/Api';
 import { GeneralPermissions } from './components/common/interfaces';
-import NoApi from './components/common/informationalComponents/NoApi';
-import GeneralError from './components/common/informationalComponents/GeneralError';
-import { Provider } from 'react-redux';
-import { store } from 'store';
-
-export const UserConfig = React.createContext(myMSALObj);
-export const Permissions = React.createContext<GeneralPermissions>({
-    admin: false,
-    canRead_PreApproved_Datasets: false,
-    canEdit_PreApproved_Datasets: false,
-    canCreateStudy: false,
-    datasetAdmin: false,
-    sponsor: false,
-    fullName: '',
-    emailAddress: '',
-    userName: ''
-});
 
 const renderApp = async (user) => {
-    await getPermissions().then((result: any) => {
-        if (result && (result.requestId || result.errors)) {
-            return ReactDOM.render(<GeneralError />, document.getElementById('root'));
-        }
-        if (result && result.admin !== undefined) {
-            return ReactDOM.render(
-                <React.StrictMode>
-                    <Provider store={store}>
-                        <UserConfig.Provider value={user}>
-                            <Permissions.Provider value={result}>
-                                <App />
-                            </Permissions.Provider>
-                        </UserConfig.Provider>
-                    </Provider>
-                </React.StrictMode>,
-                document.getElementById('root')
-            );
-        }
-        return ReactDOM.render(<NoApi />, document.getElementById('root'));
-    });
+    return ReactDOM.render(<App />, document.getElementById('root'));
 };
-
-const cyToken = localStorage.getItem('cyToken');
-
-if (cyToken && cyToken.length) {
-    const mockUser = {
-        account: {
-            name: 'MockUser',
-            roles: ''
-        }
-    };
-    renderApp(mockUser);
-} else {
-    SignInSilentRedirect();
-
-    if (myMSALObj.getAllAccounts().length > 0) {
-        renderApp(myMSALObj);
-    }
-}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
