1d18b1df9b70b9e489e7a30d005c85f85de1db22
Jone-Torgersen <jone.torgersen@bouvet.no>
2023-03-29 11:13:18 +0200

    feat: add decomission message

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <Typography>
-        Sepes is in the process of being decomissioned and is currently being removed from production.
-If you are currently in the need of Sepes for your project, please contact us.
-        </Typography>
-        <a
-          className="App-link"
-          href="https://join.slack.com/share/enQtNDk2MjMwNjA3Mzk3NS1mZjAzMzE1ZTczNzllYTRiNDJlM2NkNGI3MDJlNzMwMzAyYTFiNDYzNDNkM2ZhZjcwY2M0MmMxZDFhZjEwMjJm"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          #sepes
-        </a>
-        <a
-          className="App-link"
-          href="https://github.com/equinor/sepes-internal"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-        <a
-          className="App-link"
-          href="kton@equinor.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Contact product owner
-        </a>
-      </header>
+		 <img src="logo.svg" alt="App Logo"></img>
+      <p>Sepes is in the process of being decomissioned and is currently being removed from production.</p>
+	  <p>If you are currently in the need of Sepes for your project, please contact us.</p>
+      <ul>
+        <li><a href="https://join.slack.com/share/enQtNDk2MjMwNjA3Mzk3NS1mZjAzMzE1ZTczNzllYTRiNDJlM2NkNGI3MDJlNzMwMzAyYTFiNDYzNDNkM2ZhZjcwY2M0MmMxZDFhZjEwMjJm">
+			#Slack</a></li>
+        <li><a href="https://github.com/equinor/sepes-internal">
+			GitHub</a></li>
+        <li><a href="mailto:kton@equinor.com">Contact product owner</a></li>
+      </ul>
     </div>
   );
 }
